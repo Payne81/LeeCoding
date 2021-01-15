802ebaa9c838bed77d058b30c3fca2f7065f21a4
Payne <3334248237@qq.com>
2021-01-15 17:27:16 +0800

    Application configure

---
+++ Rust/Actix-web/2-application4.rs
@@ -0,0 +1,38 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+// 此功能可以位于其他模块中
+fn scoped_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/test")
+            .route(web::get().to(|| HttpResponse::Ok().body("test")))
+            .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+    );
+}
+
+// 此功能可以位于其他模块中
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/app")
+            .route(web::get().to(|| HttpResponse::Ok().body("app")))
+            .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+    );
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .configure(config)
+            .service(web::scope("/api").configure(scoped_config))
+            .route("/", web::get().to(|| HttpResponse::Ok().body("/")))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+/*
+效果：
+/         -> "/"
+/app      -> "app"
+/api/test -> "test"
+*/
